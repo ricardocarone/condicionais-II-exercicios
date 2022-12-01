3d8bbe2cb91b14abcd8cbd095e15447d29ff31f9
ricardocarone <ricardocaronefilho@gmail.com>
2022-12-01 13:15:53 -0300

    Exercicio-Feedback

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,23 @@
 console.log("teste 1")
 
+let numero = Number(prompt("Insira aqui um número:"))
+
+//1. Utilizando ifs aninhados
+if(numero%2===0){
+    console.log(`Número escolhido foi ${numero} e ele é divisível por 2!`)
+    }
+    if(numero%3===0){
+        console.log(`Número escolhido foi ${numero} e ele é divisível por 3!`)
+    }
+
+
+//2. Utilizando um operador lógico para unir duas operações relacionais
+
+if(numero%2===0 || numero%3===0){
+    if(numero%2===0){
+        console.log(`Número escolhido é divisível por 2`)
+    }
+    if(numero%3===0){
+        console.log(`Número escolhido é divisível por 3`)
+    }
+}
